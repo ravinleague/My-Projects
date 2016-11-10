d5b5d9de7f002bd648e918b9ed9225c24ddea6e4
RaviChalla <RaviChalla@OCO-NB04-P>
2016-11-10 17:31:57 +1030

    updated album art image

--- MVCMusicStore/MVCMusicStore/Models/Album.cs
+++ MVCMusicStore/MVCMusicStore/Models/Album.cs
@@ -26,7 +26,7 @@ namespace MVCMusicStore.Models
         //[Required(ErrorMessage ="Price is Required.")]
         //[Range(0.01,100.0,ErrorMessage ="Price must be between 0.01 and 100.00")]
         public decimal Price { get; set; }
-        public string AlbumArtUrl { get; set; } = "http://orig00.deviantart.net/12fd/f/2015/243/0/c/albumcoverfor_benprunty_fragments_sylviaritter_by_faith303-d97uftr.png";
+        public string AlbumArtUrl { get; set; } = "~/Content/Images/aart.png";
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
     }
