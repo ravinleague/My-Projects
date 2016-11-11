f87a9b9744988128534b23a702274045fad4168d
RaviChalla <RaviChalla@OCO-NB04-P>
2016-11-11 12:30:39 +1030

    added cart functionality

--- MVCMusicStore/MVCMusicStore/Models/MusicStoreEntities.cs
+++ MVCMusicStore/MVCMusicStore/Models/MusicStoreEntities.cs
@@ -10,7 +10,9 @@ namespace MVCMusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
         public System.Data.Entity.DbSet<MVCMusicStore.Models.Artist> Artists { get; set; }
+        public DbSet<Cart>Carts{ get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail>OrderDetails{ get; set; }
     }
 }
---
+++ MVCMusicStore/MVCMusicStore/ViewModel/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVCMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.ViewModel
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+        
+    }
+}
